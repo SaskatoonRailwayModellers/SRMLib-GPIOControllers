73fe39f591865885bfd22d0559d5f7fc7b7d0085
kirypto <kirypto@gmail.com>
2022-11-19 10:39:41 -0600

    Add documentation for CytronMD10C class

--- src/srmlib/gpiocontrollers/motorshields.py
+++ src/srmlib/gpiocontrollers/motorshields.py
@@ -23,6 +23,12 @@ class CytronMD10C:
     _direction: Direction
 
     def __init__(self, direction_pin: int, pulse_width_modulation_pin: int) -> None:
+        """
+        Construct a controller for a Cytron MD10C motor shield
+
+        :param direction_pin: The gpio pin connected to the motor shield's direction pin.
+        :param pulse_width_modulation_pin: The gpio pin connected to the motor shield's pwm pin.
+        """
         if getmode() != BOARD:
             # TODO kirypto 2022-Sep-17: Determine if it is safe to call setmode multiple times, and
             #  do that instead if so
@@ -38,10 +44,20 @@ class CytronMD10C:
 
     @property
     def speed(self) -> float:
+        """
+        Getter for the current speed setting as a percentage [0, 100].
+
+        :return: Returns the current speed as a percentage [0, 100].
+        """
         return self._speed
 
     @speed.setter
     def speed(self, speed_: float) -> None:
+        """
+        Setter for the current speed setting as a percentage [0, 100].
+
+        :param speed_: The new speed setting as a percentage [0, 100]
+        """
         if not (0 <= speed_ <= 100):
             raise ValueError(f"speed must be a percentage from 0 to 100 inclusive, was {speed_}")
         self._speed = speed_
@@ -49,10 +65,20 @@ class CytronMD10C:
 
     @property
     def direction(self) -> Direction:
+        """
+        Getter for the current direction setting.
+
+        :return: Returns the current direction setting.
+        """
         return self._direction
 
     @direction.setter
     def direction(self, direction_: Direction) -> None:
+        """
+        Setter for the current direction setting.
+
+        :param direction_: The new direction setting.
+        """
         if direction_ not in _VALID_DIRECTIONS:
             raise ValueError(f"{direction_} is not a valid direction, must be one of {','.join([str(direction) for direction in _VALID_DIRECTIONS])}")
         self._direction = direction_
